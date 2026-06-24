dbf682ebac10d86715bcc575249744c2296765a1
Elinar123 <adrian.ranile@gmail.com>
2026-06-24 11:00:35 +0800

    Finished code of selection sort

--- fcc20.js
+++ fcc20.js
@@ -1,21 +1,19 @@
-//SELECTIONSORT
+//SELECTION SORT
 function selectionSort(num){
-    for(let i = 0; i < num.length;i++){
-         let minimum = i;
-         let screenshot;
-        for(let j = i + 1; j < num.length; j++){
-            if(num[j] < num[minimum]){
-                screenshot = num[j];
+    for(let i = 0; i < num.length; i++){
+        let minIndex = i;
+        for(let j = 0 + i; j < num.length; j++){
+            if(num[j] < num[minIndex]){
+                minIndex = j;
             }
-            
-        } 
-        num[screenshot] =  num[minimum];
-        num[minimum] = num[screenshot];
-
-
+        }
+        let temp = num[i];
+        num[i] = num[minIndex];
+        num[minIndex] = temp;
     }
     return num;
 }
+
 
 const test = selectionSort([7, 4, 6, 9, 3, 6, 2]);
 console.log(test);
