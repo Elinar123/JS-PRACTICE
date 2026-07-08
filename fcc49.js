154b0fc3d9f00afaf5ee6c19a28f8d6983269bbf
Elinar123 <adrian.ranile@gmail.com>
2026-07-08 09:48:04 +0800

    Built a simple book organizer using filter and sort method

---
+++ fcc49.js
@@ -0,0 +1,30 @@
+//BUILD A BOOK ORGANIZER
+const books = [
+  {
+    title: "The Great Gatsby",
+    authorName: "F. Scott Fitzgerald",
+    releaseYear: 1925
+  },
+  {
+    title: "To Kill a Mockingbird",
+    authorName: "Harper Lee",
+    releaseYear: 1960
+  },
+  {
+    title: "1984",
+    authorName: "George Orwell",
+    releaseYear: 1949
+  }
+];
+
+function sortByYear(book1, book2) {
+    if(book1.releaseYear < book2.releaseYear) return -1;
+    if(book1.releaseYear > book2.releaseYear) return 1;
+    if(book1.releaseYear === book2.releaseYear) return 0;
+}
+
+let filteredBooks = books.filter(book => book.releaseYear < 1950);
+
+filteredBooks.sort(sortByYear);
+
+console.log(filteredBooks);
